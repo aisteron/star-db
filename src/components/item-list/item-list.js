4e713a1c241c2f20c9bda21b8dc01b339f8c5288
unknown <timotheus@list.ru>
2019-05-22 21:03:18 +0300

    100

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,40 +1,32 @@
 import React from 'react';
 
-import './item-list.css';
-import {withData} from "../hoc-helpers";
-
+import { withData } from '../hoc-helpers';
 import SwapiService from "../../services/swapi-service";
+import './item-list.css';
 
 const ItemList = (props) => {
 
-    const { data, onItemSelected, children: renderLabel  } = props;
+  const { data, onItemSelected, children: renderLabel } = props;
 
-    const items = data.map((item)=>{
-
-        const {id} = item;
-        const label = renderLabel(item);
-
+  const items = data.map((item) => {
+    const { id } = item;
+    const label = renderLabel(item);
 
     return (
-      <span className="list-group-item list-group-item-action"
-        key={id}
-        onClick={()=> onItemSelected(id)}
-
-      >
+      <li className="list-group-item"
+          key={id}
+          onClick={() => onItemSelected(id)}>
         {label}
-      </span>
+      </li>
     );
-    });
+  });
 
-    return (
-        <div className="list-group">
-            {items}
-        </div>
-    );
-  };
-
-
-
+  return (
+    <ul className="item-list list-group">
+      {items}
+    </ul>
+  );
+};
 
 const { getAllPeople } = new SwapiService();
 
